be27e6ab92e0495c89b37420b6a1c57c81c33462
113putthimethmalan-jpg <11.3.putthimethmalan@gmail.com>
2026-05-03 10:36:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,128 +1,411 @@
-window.requestAnimationFrame =
-    window.__requestAnimationFrame ||
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    (function () {
-        return function (callback, element) {
-            var lastTime = element ? element.__lastTime : 0;
-            if (lastTime === undefined) lastTime = 0;
-            var currTime = Date.now();
-            var timeToCall = Math.max(1, 33 - (currTime - lastTime));
-            window.setTimeout(callback, timeToCall);
-            if (element) element.__lastTime = currTime + timeToCall;
-        };
-    })();
+/* Poly Heart model by Quaternius [CC0] (https://creativecommons.org/publicdomain/zero/1.0/) via Poly Pizza (https://poly.pizza/m/1yCRUwFnwX)
+ */
 
-window.isDevice = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(((navigator.userAgent || navigator.vendor || window.opera)).toLowerCase()));
-
-var init = function () {
-    var canvas = document.getElementById('heart');
-    var ctx = canvas.getContext('2d');
-    var width = canvas.width = window.innerWidth;
-    var height = canvas.height = window.innerHeight;
-    var rand = Math.random;
-
-    ctx.fillStyle = "rgba(0,0,0,1)";
-    ctx.fillRect(0, 0, width, height);
-
-    var heartPosition = function (rad) {
-        return [Math.pow(Math.sin(rad), 3), -(15 * Math.cos(rad) - 5 * Math.cos(2 * rad) - 2 * Math.cos(3 * rad) - Math.cos(4 * rad))];
+import * as THREE from "https://cdn.skypack.dev/three@0.135.0";
+import { gsap } from "https://cdn.skypack.dev/gsap@3.8.0";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.135.0/examples/jsm/loaders/GLTFLoader";
+class World {
+  constructor({
+    canvas,
+    width,
+    height,
+    cameraPosition,
+    fieldOfView = 75,
+    nearPlane = 0.1,
+    farPlane = 100 }) {
+    this.parameters = {
+      count: 1500,
+      max: 12.5 * Math.PI,
+      a: 2,
+      c: 4.5
     };
 
-    var scaleAndTranslate = function (pos, sx, sy, dx, dy) {
-        return [dx + pos[0] * sx, dy + pos[1] * sy];
-    };
+    this.textureLoader = new THREE.TextureLoader();
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x16000a);
+    this.clock = new THREE.Clock();
+    this.data = 0;
+    this.time = { current: 0, t0: 0, t1: 0, t: 0, frequency: 0.0005 };
+    this.angle = { x: 0, z: 0 };
+    this.width = width || window.innerWidth;
+    this.height = height || window.innerHeight;
+    this.aspectRatio = this.width / this.height;
+    this.fieldOfView = fieldOfView;
+    this.camera = new THREE.PerspectiveCamera(
+      fieldOfView,
+      this.aspectRatio,
+      nearPlane,
+      farPlane);
 
-    window.onresize = function () {
-        width = canvas.width = window.innerWidth;
-        height = canvas.height = window.innerHeight;
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctx.fillRect(0, 0, width, height);
-    };
+    this.camera.position.set(
+      cameraPosition.x,
+      cameraPosition.y,
+      cameraPosition.z);
 
-    var pointsOrigin = [];
-    var dr = window.isDevice ? 0.2 : 0.1;
-    for (var i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 210, 13, 0, 0));
-    for (var i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 150, 9, 0, 0));
-    for (var i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 90, 5, 0, 0));
-    
-    var heartPointsCount = pointsOrigin.length;
-    var targetPoints = [];
-    var pulse = function (kx, ky) {
-        for (var i = 0; i < pointsOrigin.length; i++) {
-            targetPoints[i] = [];
-            targetPoints[i][0] = kx * pointsOrigin[i][0] + width / 2;
-            targetPoints[i][1] = ky * pointsOrigin[i][1] + height / 2;
-        }
-    };
+    this.scene.add(this.camera);
+    this.renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    });
 
-    var e = [];
-    for (var i = 0; i < heartPointsCount; i++) {
-        var x = rand() * width;
-        var y = rand() * height;
-        e[i] = {
-            vx: 0, vy: 0, R: 2, speed: rand() + 5,
-            q: ~~(rand() * heartPointsCount),
-            D: 2 * (rand() - 0.5), force: 0.2 * rand() + 0.7,
-            f: "hsla(0," + ~~(40 * rand() + 60) + "%," + ~~(40 * rand() + 20) + "%,.3)",
-            trace: []
-        };
-        for (var k = 0; k < (window.isDevice ? 20 : 50); k++) e[i].trace[k] = { x: x, y: y };
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+    this.renderer.setPixelRatio(this.pixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.timer = 0;
+    this.addToScene();
+    this.addButton();
+
+    this.render();
+    this.listenToResize();
+    this.listenToMouseMove();
+  }
+  start() { }
+  render() {
+    this.renderer.render(this.scene, this.camera);
+    this.composer && this.composer.render();
+  }
+  loop() {
+    this.time.elapsed = this.clock.getElapsedTime();
+    this.time.delta = Math.min(
+      60,
+      (this.time.current - this.time.elapsed) * 1000);
+
+    if (this.analyser && this.isRunning) {
+      this.time.t = this.time.elapsed - this.time.t0 + this.time.t1;
+      this.data = this.analyser.getAverageFrequency();
+      this.data *= this.data / 2000;
+      this.angle.x += this.time.delta * 0.001 * 0.63;
+      this.angle.z += this.time.delta * 0.001 * 0.39;
+      const justFinished = this.isRunning && !this.sound.isPlaying;
+      if (justFinished) {
+        this.time.t1 = this.time.t;
+        this.audioBtn.disabled = false;
+        this.isRunning = false;
+        const tl = gsap.timeline();
+        this.angle.x = 0;
+        this.angle.z = 0;
+        tl.to(this.camera.position, {
+          x: 0,
+          z: 4.5,
+          duration: 4,
+          ease: "expo.in"
+        });
+
+        tl.to(this.audioBtn, {
+          opacity: () => 1,
+          duration: 1,
+          ease: "power1.out"
+        });
+
+      } else {
+        this.camera.position.x = Math.sin(this.angle.x) * this.parameters.a;
+        this.camera.position.z = Math.min(
+          Math.max(Math.cos(this.angle.z) * this.parameters.c, 1.75),
+          6.5);
+
+      }
     }
+    this.camera.lookAt(this.scene.position);
+    if (this.heartMaterial) {
+      this.heartMaterial.uniforms.uTime.value +=
+        this.time.delta * this.time.frequency * (1 + this.data * 0.2);
+    }
+    if (this.model) {
+      this.model.rotation.y -= 0.0005 * this.time.delta * (1 + this.data);
+    }
+    if (this.snowMaterial) {
+      this.snowMaterial.uniforms.uTime.value +=
+        this.time.delta * 0.0004 * (1 + this.data);
+    }
+    this.render();
 
-    var loop = function () {
-        var m = Math.pow(Math.sin(Date.now() * 0.001), 2);
-        var p = 1 + 0.15 * m;
-        pulse(p, p);
+    this.time.current = this.time.elapsed;
+    requestAnimationFrame(this.loop.bind(this));
+  }
+  listenToResize() {
+    window.addEventListener("resize", () => {
+      // Update sizes
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
 
-        ctx.fillStyle = "rgba(0,0,0,.1)";
-        ctx.fillRect(0, 0, width, height);
+      // Update camera
+      this.camera.aspect = this.width / this.height;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(this.width, this.height);
+    });
+  }
+  listenToMouseMove() {
+    window.addEventListener("mousemove", e => {
+      const x = e.clientX;
+      const y = e.clientY;
+      gsap.to(this.camera.position, {
+        x: gsap.utils.mapRange(0, window.innerWidth, 0.2, -0.2, x),
+        y: gsap.utils.mapRange(0, window.innerHeight, 0.2, -0.2, -y)
+      });
 
-        for (var i = 0; i < e.length; i++) {
-            var u = e[i];
-            var q = targetPoints[u.q];
-            var dx = u.trace[0].x - q[0];
-            var dy = u.trace[0].y - q[1];
-            var length = Math.sqrt(dx * dx + dy * dy);
-            
-            if (10 > length) {
-                if (0.95 < rand()) {
-                    u.q = ~~(rand() * heartPointsCount);
-                } else {
-                    if (0.99 < rand()) u.D *= -1;
-                    u.q += u.D;
-                    u.q %= heartPointsCount;
-                    if (0 > u.q) u.q += heartPointsCount;
-                }
-            }
-
-            u.vx += (-dx / length) * u.speed;
-            u.vy += (-dy / length) * u.speed;
-            u.trace[0].x += u.vx;
-            u.trace[0].y += u.vy;
-            u.vx *= u.force;
-            u.vy *= u.force;
-
-            for (var k = 0; k < u.trace.length - 1;) {
-                var T = u.trace[k];
-                var N = u.trace[++k];
-                N.x -= 0.4 * (N.x - T.x);
-                N.y -= 0.4 * (N.y - T.y);
-            }
-
-            ctx.fillStyle = u.f;
-            for (var k = 0; k < u.trace.length; k++) {
-                ctx.fillRect(u.trace[k].x, u.trace[k].y, 1, 1);
-            }
+    });
+  }
+  addHeart() {
+    this.heartMaterial = new THREE.ShaderMaterial({
+      fragmentShader: document.getElementById("fragmentShader").textContent,
+      vertexShader: document.getElementById("vertexShader").textContent,
+      uniforms: {
+        uTime: { value: 0 },
+        uSize: { value: 0.2 },
+        uTex: {
+          value: new THREE.TextureLoader().load(
+            "https://assets.codepen.io/74321/heart.png")
         }
-        window.requestAnimationFrame(loop, canvas);
-    };
-    loop();
-};
+      },
 
-// บรรทัดนี้สำคัญที่สุด คือการสั่งให้โค้ดเริ่มทำงาน
-if (document.readyState === 'complete') init();
-else window.addEventListener('load', init);
+
+
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    const count = this.parameters.count; //2000
+    const scales = new Float32Array(count * 1);
+    const colors = new Float32Array(count * 3);
+    const speeds = new Float32Array(count);
+    const randoms = new Float32Array(count);
+    const randoms1 = new Float32Array(count);
+    const colorChoices = [
+      "white",
+      "red",
+      "pink",
+      "crimson",
+      "hotpink",
+      "green"];
+
+
+    const squareGeometry = new THREE.PlaneGeometry(1, 1);
+    this.instancedGeometry = new THREE.InstancedBufferGeometry();
+    Object.keys(squareGeometry.attributes).forEach(attr => {
+      this.instancedGeometry.attributes[attr] = squareGeometry.attributes[attr];
+    });
+    this.instancedGeometry.index = squareGeometry.index;
+    this.instancedGeometry.maxInstancedCount = count;
+
+    for (let i = 0; i < count; i++) {
+      const phi = Math.random() * Math.PI * 2;
+      const i3 = 3 * i;
+      randoms[i] = Math.random();
+      randoms1[i] = Math.random();
+      scales[i] = Math.random() * 0.35;
+      const colorIndex = Math.floor(Math.random() * colorChoices.length);
+      const color = new THREE.Color(colorChoices[colorIndex]);
+      colors[i3 + 0] = color.r;
+      colors[i3 + 1] = color.g;
+      colors[i3 + 2] = color.b;
+      speeds[i] = Math.random() * this.parameters.max;
+    }
+    this.instancedGeometry.setAttribute(
+      "random",
+      new THREE.InstancedBufferAttribute(randoms, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "random1",
+      new THREE.InstancedBufferAttribute(randoms1, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "aScale",
+      new THREE.InstancedBufferAttribute(scales, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "aSpeed",
+      new THREE.InstancedBufferAttribute(speeds, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "aColor",
+      new THREE.InstancedBufferAttribute(colors, 3, false));
+
+    this.heart = new THREE.Mesh(this.instancedGeometry, this.heartMaterial);
+    console.log(this.heart);
+    this.scene.add(this.heart);
+  }
+  addToScene() {
+    this.addModel();
+    this.addHeart();
+    this.addSnow();
+  }
+  async addModel() {
+    this.model = await this.loadObj(
+      "https://assets.codepen.io/74321/heart.glb");
+
+    this.model.scale.set(0.01, 0.01, 0.01);
+    this.model.material = new THREE.MeshMatcapMaterial({
+      matcap: this.textureLoader.load(
+        "https://assets.codepen.io/74321/3.png",
+        () => {
+          gsap.to(this.model.scale, {
+            x: 0.35,
+            y: 0.35,
+            z: 0.35,
+            duration: 1.5,
+            ease: "Elastic.easeOut"
+          });
+
+        }),
+
+      color: "#ff89aC"
+    });
+
+    this.scene.add(this.model);
+  }
+  addButton() {
+    this.audioBtn = document.querySelector("button");
+    this.audioBtn.addEventListener("click", () => {
+      this.audioBtn.disabled = true;
+      if (this.analyser) {
+        this.sound.play();
+        this.time.t0 = this.time.elapsed;
+        this.data = 0;
+        this.isRunning = true;
+        gsap.to(this.audioBtn, {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.out"
+        });
+
+      } else {
+        this.loadMusic().then(() => {
+          console.log("music loaded");
+        });
+      }
+    });
+  }
+  loadObj(path) {
+    const loader = new GLTFLoader();
+    return new Promise(resolve => {
+      loader.load(
+        path,
+        response => {
+          resolve(response.scene.children[0]);
+        },
+        xhr => { },
+        err => {
+          console.log(err);
+        });
+
+    });
+  }
+  loadMusic() {
+    return new Promise(resolve => {
+      const listener = new THREE.AudioListener();
+      this.camera.add(listener);
+      // create a global audio source
+      this.sound = new THREE.Audio(listener);
+      const audioLoader = new THREE.AudioLoader();
+      audioLoader.load(
+        "https://assets.codepen.io/74321/ukulele.mp3",
+        buffer => {
+          this.sound.setBuffer(buffer);
+          this.sound.setLoop(false);
+          this.sound.setVolume(0.5);
+          this.sound.play();
+          this.analyser = new THREE.AudioAnalyser(this.sound, 32);
+          // get the average frequency of the sound
+          const data = this.analyser.getAverageFrequency();
+          this.isRunning = true;
+          this.t0 = this.time.elapsed;
+          resolve(data);
+        },
+        progress => {
+          gsap.to(this.audioBtn, {
+            opacity: () => 1 - progress.loaded / progress.total,
+            duration: 1,
+            ease: "power1.out"
+          });
+
+        },
+
+        error => {
+          console.log(error);
+        });
+
+    });
+  }
+  addSnow() {
+    this.snowMaterial = new THREE.ShaderMaterial({
+      fragmentShader: document.getElementById("fragmentShader1").textContent,
+      vertexShader: document.getElementById("vertexShader1").textContent,
+      uniforms: {
+        uTime: { value: 0 },
+        uSize: { value: 0.3 },
+        uTex: {
+          value: new THREE.TextureLoader().load(
+            "https://assets.codepen.io/74321/heart.png")
+        }
+      },
+
+
+
+      depthWrite: false,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+
+    const count = 550;
+    const scales = new Float32Array(count * 1);
+    const colors = new Float32Array(count * 3);
+    const phis = new Float32Array(count);
+    const randoms = new Float32Array(count);
+    const randoms1 = new Float32Array(count);
+    const colorChoices = ["red", "pink", "hotpink", "green"];
+
+    const squareGeometry = new THREE.PlaneGeometry(1, 1);
+    this.instancedGeometry = new THREE.InstancedBufferGeometry();
+    Object.keys(squareGeometry.attributes).forEach(attr => {
+      this.instancedGeometry.attributes[attr] = squareGeometry.attributes[attr];
+    });
+    this.instancedGeometry.index = squareGeometry.index;
+    this.instancedGeometry.maxInstancedCount = count;
+
+    for (let i = 0; i < count; i++) {
+      const phi = (Math.random() - 0.5) * 10;
+      const i3 = 3 * i;
+      phis[i] = phi;
+      randoms[i] = Math.random();
+      randoms1[i] = Math.random();
+      scales[i] = Math.random() * 0.35;
+      const colorIndex = Math.floor(Math.random() * colorChoices.length);
+      const color = new THREE.Color(colorChoices[colorIndex]);
+      colors[i3 + 0] = color.r;
+      colors[i3 + 1] = color.g;
+      colors[i3 + 2] = color.b;
+    }
+    this.instancedGeometry.setAttribute(
+      "phi",
+      new THREE.InstancedBufferAttribute(phis, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "random",
+      new THREE.InstancedBufferAttribute(randoms, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "random1",
+      new THREE.InstancedBufferAttribute(randoms1, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "aScale",
+      new THREE.InstancedBufferAttribute(scales, 1, false));
+
+    this.instancedGeometry.setAttribute(
+      "aColor",
+      new THREE.InstancedBufferAttribute(colors, 3, false));
+
+    this.snow = new THREE.Mesh(this.instancedGeometry, this.snowMaterial);
+    this.scene.add(this.snow);
+  }
+}
+
+
+const world = new World({
+  canvas: document.querySelector("canvas.webgl"),
+  cameraPosition: { x: 0, y: 0, z: 4.5 }
+});
+
+
+world.loop();
